5915c4960cbb217527cf5fafe68eb6e0003b015c
Exokun14 <clarencejoven1456@gmail.com>
2024-11-08 06:53:38 +0800

    Commit?

---
+++ src/themes/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const themeOptions = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#00635d',
+    },
+    secondary: {
+      main: '#3c3c3c',
+    },
+  },
+});
